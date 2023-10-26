beb2aae3e43d9b6aa6442321843b2f960ee7afc8
wuliya <email@wuliya.top>
2023-10-26 17:31:56 +0800

    var:1.2

--- zx.php
+++ zx.php
@@ -1,6 +1,6 @@
 
 <?php
-// 原作者为MCNewsTools，由GamerNoTitle二次修改，由MING SERVER腐竹烧饼修改对接官网，模板套用的HTML5UP的HTML模板，请在使用该程序的时候不要删除版权信息，Thanks♪(･ω･)ﾉ！
+// 原作者为MCNewsTools，由GamerNoTitle二次修改，由MING SERVER腐竹修改对接官网，模板套用的HTML5UP的HTML模板，请在使用该程序的时候不要删除版权信息，Thanks♪(･ω･)ﾉ！
 $host = "101.42.237.97"; //设置服务器域名或 IP
 $port = "28652"; //设置服务器端口，可以直接用服务器的链接端口，也可以使用query端口（UDP协议）
 $name = 'MING SERVER'; // 服务器名字，会显示在标签页的标题和网站打头的大标题处
